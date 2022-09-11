ee7be7e4190bcd007d3f074e5609b63afae1e614
honey the codewitch <gaztoast@gmail.com>
2022-09-11 07:11:38 -0700

    updated src to latest bits, and adapted for v8.3.1

---
+++ lib/fs_cbfs/lv_fs_cbfs.h
@@ -0,0 +1,13 @@
+#ifndef LV_FS_CBFS_H
+#define LV_FS_CBFS_H
+#include <lvgl.h>
+#include <stddef.h>
+#ifdef __cplusplus
+extern "C" {
+#endif
+void lv_fs_cbfs_init(void);
+lv_fs_res_t lv_fs_cbfs_create(char * out_path, size_t out_path_size, const void * data, size_t data_size);
+#ifdef __cplusplus
+}
+#endif
+#endif // LV_FS_CBFS_H
